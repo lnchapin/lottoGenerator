269caef40fbaa50bc32d5f73c83435ae301a7e08
Lindsay Chapin <lnchapin@gmail.com>
2019-06-24 14:56:37 -0600

    fixed oblonging

--- src/Components/Ball.js
+++ src/Components/Ball.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Col } from 'reactstrap'
 
-const Ball = styled(Col)`
+const Ball = styled.div`
   background-color: tomato;
   border-radius: 50%;
   color: white;
   display: inline-block;
   width: 3em;
-  height: 2.25em;
+  height: 3em;
   padding-top: 0.75em;
   text-align: center;
   margin-right: 0.5em;
   font-weight: bold;
   font-size: 1.5em;
+  margin: auto;
 `
 
 const LottoBall = props => {
   console.log(props);
   return(
-    <Ball>
-      {props.number}
-    </Ball>
+      <Ball>
+        {props.number}
+      </Ball>
   )
 }
 
